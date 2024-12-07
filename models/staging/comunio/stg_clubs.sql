f60749d6bef792d62719bcab129ba9b4b07e54ee
Fabian Roth <roth-f@outlook.de>
2024-12-07 16:38:32 +0000

    Changed all refrences to copy of raw comunio player table. Debugged Test unique test failure of players table. Added description to every table.

--- models/staging/comunio/stg_clubs.sql
+++ models/staging/comunio/stg_clubs.sql
@@ -1,9 +1,9 @@
-with clubs as (
-    select distinct
+WITH clubs AS (
+    SELECT DISTINCT
         CAST(SPLIT_PART(club_id, '-', 1) AS INTEGER) AS club_id_nr,
         club_id,
         club_title 
-    from {{ref("raw_comunio_players_and_market_values")}}
+    FROM {{ref("raw_comunio_players_and_market_values")}}
 )
 
-select * from clubs
+SELECT * FROM clubs
--- models/staging/comunio/stg_players.sql
+++ models/staging/comunio/stg_players.sql
@@ -1,11 +1,19 @@
-with players as (
-    select distinct
-        comstats_data_player_id as player_id,
-        player_name,
-        position,
-        comunio_profile_url,
-        club_id
-    from {{ source('comunio', 'raw_comunio_players') }}
+WITH players AS (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY player_id
+            ORDER BY loaded_date DESC
+        ) AS row_number
+    FROM {{ ref('raw_comunio_players_and_market_values') }}
 )
 
-select * from players
+SELECT DISTINCT
+    player_id,
+    player_name,
+    position,
+    comunio_profile_url,
+    club_id,
+    loaded_date 
+FROM players
+WHERE row_number = 1
